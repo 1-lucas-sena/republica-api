d6260949c87fc9f109d42e48da22835b14ff10d8
1LucasSena <seu-email-vinculado-a-1LucasSena>
2026-04-15 16:30:37 -0300

    chore: setup inicial do projeto com Spring Initializr

---
+++ src/main/java/com/lucassena/republica_api/RepublicaApiApplication.java
@@ -0,0 +1,13 @@
+package com.lucassena.republica_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepublicaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepublicaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucassena/republica_api/RepublicaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucassena.republica_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepublicaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
